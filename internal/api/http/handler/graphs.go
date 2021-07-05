e2570a2a31201f4bb19072b68c9f3e684971c5a5
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-07-05 12:30:13 +0300

    refactor: move fetching data from api to the func

--- internal/api/http/handler/graphs.go
+++ internal/api/http/handler/graphs.go
@@ -2,74 +2,26 @@ package handler
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
 
 	api "github.com/gxravel/bus-routes-visualizer/internal/api/http"
 	busroutesapi "github.com/gxravel/bus-routes-visualizer/internal/busroutesapi"
-	busroutesapiV1 "github.com/gxravel/bus-routes-visualizer/internal/busroutesapi/v1"
-	ierr "github.com/gxravel/bus-routes-visualizer/internal/errors"
 )
 
 func (s *Server) getGraph(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	url, err := api.ParseGraphsRequest(r, s.busroutesAPI)
+	url, err := api.ParseGraphsRequest(r, s.busroutesAPI+busroutesapi.RouteForBuses)
 	if err != nil {
 		api.RespondError(ctx, w, err)
 		return
 	}
 
-	logger := s.logger.WithStr("url", url)
-	logger.Debug("going to buses")
-
-	code, data, err := busroutesapi.GetItems(url, busroutesapi.TypeBuses)
+	routes, err := busroutesapi.GetRoutesDetailed(ctx, s.busroutesAPI, url)
 	if err != nil {
-		if err == ierr.ErrProviderAPI {
-			api.RespondJSON(ctx, w, code, data)
-			return
-		}
 		api.RespondError(ctx, w, err)
 		return
 	}
-	buses, ok := data.(*busroutesapiV1.RangeBusesResponse)
-	if !ok {
-		logger.WithField("data", data).Error("expect RangeBusesResponse")
-		api.RespondError(ctx, w, ierr.ErrInternalServer)
-		return
-	}
-	if buses.Total == 0 {
-		api.RespondEmptyItems(ctx, w)
-		return
-	}
-
-	urlBuilder := strings.Builder{}
-	urlBuilder.WriteString(s.busroutesAPI + "/routes/detailed?bus_ids=")
-	for _, bus := range buses.Buses {
-		urlBuilder.WriteString(strconv.FormatInt(bus.ID, 10))
-		urlBuilder.WriteString(",")
-	}
-	url = urlBuilder.String()[:urlBuilder.Len()-1]
-
-	logger = s.logger.WithStr("url", url)
-	logger.Debug("going to routes")
-
-	code, data, err = busroutesapi.GetItems(url, busroutesapi.TypeRoutes)
-	if err != nil {
-		if err == ierr.ErrProviderAPI {
-			api.RespondJSON(ctx, w, code, data)
-			return
-		}
-		api.RespondError(ctx, w, err)
-		return
-	}
-	routes, ok := data.(*busroutesapiV1.RangeRoutesResponse)
-	if !ok {
-		logger.WithField("data", data).Error("expect RangeRoutesResponse")
-		api.RespondError(ctx, w, err)
-		return
-	}
-	if routes.Total == 0 {
+	if routes == nil {
 		api.RespondEmptyItems(ctx, w)
 		return
 	}
--- internal/busroutesapi/busroutesapi.go
+++ internal/busroutesapi/busroutesapi.go
@@ -1,12 +1,22 @@
 package busroutesapi
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
 	api "github.com/gxravel/bus-routes-visualizer/internal/api/http"
 	"github.com/gxravel/bus-routes-visualizer/internal/busroutesapi/v1"
-	"github.com/gxravel/bus-routes-visualizer/internal/errors"
+	ierr "github.com/gxravel/bus-routes-visualizer/internal/errors"
+	log "github.com/gxravel/bus-routes-visualizer/internal/logger"
+	"github.com/gxravel/bus-routes-visualizer/internal/visualizercontext"
+)
+
+const (
+	RouteForBuses  = "/buses"
+	RouteForRoutes = "/routes/detailed"
 )
 
 type ItemsType uint8
@@ -16,12 +26,72 @@ const (
 	TypeRoutes
 )
 
-// GetItems makes request to the busroutes api
-// and converts a RangeItemsResponse to the specified type.
-func GetItems(url string, itemsType ItemsType) (int, interface{}, error) {
-	response, err := http.Get(url)
+// GetRoutesDetailed makes 2 requests to the API:
+// 1) /buses for receiving buses ids
+// 2) /routes/detailed for receiving routes.
+func GetRoutesDetailed(ctx context.Context, api string, url string) (*busroutesapi.RangeRoutesResponse, error) {
+	logger := log.FromContext(ctx).WithStr("url", url)
+	logger.Debug("going for buses")
+
+	setToken := false
+
+	data, err := getItems(ctx, url, setToken, TypeBuses)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
+	}
+	buses, ok := data.(*busroutesapi.RangeBusesResponse)
+	if !ok {
+		logger.WithField("data", data).Error("expect RangeBusesResponse")
+		return nil, ierr.ErrInternalServer
+	}
+	if buses.Total == 0 {
+		return nil, nil
+	}
+
+	urlBuilder := strings.Builder{}
+	urlBuilder.WriteString(api + RouteForRoutes + "?bus_ids=")
+	for _, bus := range buses.Buses {
+		urlBuilder.WriteString(strconv.FormatInt(bus.ID, 10))
+		urlBuilder.WriteString(",")
+	}
+	url = urlBuilder.String()[:urlBuilder.Len()-1]
+
+	logger = log.FromContext(ctx).WithStr("url", url)
+	logger.Debug("going for routes")
+
+	setToken = true
+
+	data, err = getItems(ctx, url, setToken, TypeRoutes)
+	if err != nil {
+		return nil, err
+	}
+	routes, ok := data.(*busroutesapi.RangeRoutesResponse)
+	if !ok {
+		logger.WithField("data", data).Error("expect RangeRoutesResponse")
+		return nil, err
+	}
+	if routes.Total == 0 {
+		return nil, nil
+	}
+
+	return routes, nil
+}
+
+// getItems makes request to the busroutes api
+// and converts a RangeItemsResponse to the specified type.
+func getItems(ctx context.Context, url string, setToken bool, itemsType ItemsType) (interface{}, error) {
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if setToken {
+		setAuthToken(ctx, request)
+	}
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
 	}
 
 	var itemsResponse = &api.Response{}
@@ -36,15 +106,20 @@ func GetItems(url string, itemsType ItemsType) (int, interface{}, error) {
 	}
 
 	if err := processResponse(response, itemsResponse); err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
 	if itemsResponse.Error != nil {
-		itemsResponse.Data = nil
-		return response.StatusCode, itemsResponse, errors.ErrProviderAPI
+		return nil, ierr.NewTypedError(
+			itemsResponse.Error.Reason.RType,
+			ierr.NewProviderAPIError(
+				itemsResponse.Error.Reason.Err,
+				response.StatusCode,
+			),
+		)
 	}
 
-	return 0, itemsResponse.Data, nil
+	return itemsResponse.Data, nil
 }
 
 func processResponse(r *http.Response, data interface{}) error {
@@ -52,4 +127,8 @@ func processResponse(r *http.Response, data interface{}) error {
 		return err
 	}
 	return nil
+}
+
+func setAuthToken(ctx context.Context, r *http.Request) {
+	r.Header.Set("Authorization", "Bearer "+visualizercontext.GetToken(ctx))
 }
