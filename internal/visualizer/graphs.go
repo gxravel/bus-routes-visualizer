e1ad945a3aa5ed521e7177cef13c5c62c32e78b7
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-07-06 12:26:38 +0300

    refactor(graphs): work with file on visualiser lvl

--- internal/visualizer/graphs.go
+++ internal/visualizer/graphs.go
@@ -1,12 +1,16 @@
 package visualizer
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"os"
 
 	"github.com/gxravel/bus-routes-visualizer/internal/busroutesapi"
 	v1 "github.com/gxravel/bus-routes-visualizer/internal/busroutesapi/v1"
 	"github.com/gxravel/bus-routes-visualizer/internal/drawing"
 	ierr "github.com/gxravel/bus-routes-visualizer/internal/errors"
+	"github.com/gxravel/bus-routes-visualizer/internal/logger"
 	"github.com/gxravel/bus-routes-visualizer/internal/model"
 )
 
@@ -28,9 +32,28 @@ func (r *Visualizer) GetRoutesDetailed(ctx context.Context, url string) ([]*v1.R
 }
 
 // DrawGraph returns path to the graph image.
-func (r *Visualizer) DrawGraph(routes []*v1.RouteDetailed) (string, error) {
+func (r *Visualizer) DrawGraph(ctx context.Context, routes []*v1.RouteDetailed) (int64, []byte, error) {
 	graphName := routes[0].City + "_" + routes[0].Bus
-	return drawing.DrawRoutes(graphName, routes)
+	path, err := drawing.DrawRoutes(graphName, routes)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		logger.FromContext(ctx).WithErr(err).Error("open file")
+		return 0, nil, err
+	}
+	defer file.Close()
+
+	buf := &bytes.Buffer{}
+	size, err := io.Copy(buf, file)
+	if err != nil {
+		logger.FromContext(ctx).WithErr(err).Error("write to buffer")
+		return 0, nil, err
+	}
+
+	return size, buf.Bytes(), nil
 }
 
 func toDBRoutes(routes []*v1.RouteDetailed) []*model.Route {
