cbd05254bfbb4825e6573229b2ec9ddc067da67e
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-07-06 14:01:34 +0300

    refactor(db): delete route_points by filter

--- internal/dataprovider/route_point.go
+++ internal/dataprovider/route_point.go
@@ -10,5 +10,19 @@ type RoutePointStore interface {
 	WithTx(*Tx) RoutePointStore
 	Add(ctx context.Context, points ...*model.RoutePoint) error
 	Update(ctx context.Context, point *model.RoutePoint) error
-	Delete(ctx context.Context, id int64) error
+	Delete(ctx context.Context, filter *RoutePointFilter) error
+}
+
+type RoutePointFilter struct {
+	IDs []int64
+}
+
+func NewRoutePointFilter() *RoutePointFilter {
+	return &RoutePointFilter{}
+}
+
+// ByIDs filters by route_point.id
+func (f *RoutePointFilter) ByIDs(ids ...int64) *RoutePointFilter {
+	f.IDs = ids
+	return f
 }
